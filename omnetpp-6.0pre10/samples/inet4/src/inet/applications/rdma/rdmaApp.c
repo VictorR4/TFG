ccfc5c9c83d2fca84915bd26dc8fa96aee0b88c9
Victor <victormanuelromero99@gmail.com>
2021-07-07 13:47:13 +0200

    Elementos de inet

---
+++ omnetpp-6.0pre10/samples/inet4/src/inet/applications/rdma/rdmaApp.c
@@ -0,0 +1,10 @@
+/*
+ * rdmaApp.c
+ *
+ *  Created on: Jun 18, 2021
+ *      Author: vr4
+ */
+
+
+#include "inet/applications/rdma/rdmaApp.h"
+
---
+++ omnetpp-6.0pre10/samples/inet4/src/inet/applications/rdma/rdmaApp.h
@@ -0,0 +1,15 @@
+/*
+ * rdmaApp.h
+ *
+ *  Created on: Jun 18, 2021
+ *      Author: vr4
+ */
+
+#ifndef INET_APPLICATIONS_RDMA_RDMAAPP_H_
+#define INET_APPLICATIONS_RDMA_RDMAAPP_H_
+
+
+
+
+
+#endif /* INET_APPLICATIONS_RDMA_RDMAAPP_H_ */
