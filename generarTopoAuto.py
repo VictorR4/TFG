09d53fe8fb6269b69f58a58b46133387574a1fd9
Victor <victormanuelromero99@gmail.com>
2021-09-07 13:40:05 +0200

    Archivos de generación de topologias

---
+++ generarTopoAuto.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+# hTotales = número de host totales
+
+import math
+import sys
+
+if len(sys.argv) != 2:
+	print('El formato de ejecución debe ser ./generarTopo.py num_host_totales')
+
+hTotales = int(sys.argv[1])
+print('types:')
+print('        channel cable extends DatarateChannel')
+print('        {')
+print('            datarate = 40Gbps;')
+print('            delay = 1us;')
+print('        }')
+
+#Declaración host y switches
+print('submodules:')
+puertos_switch = math.sqrt(hTotales/2)
+if '.0' in str(puertos_switch):
+    puertos_switch = int(puertos_switch)
+
+else:
+    puertos_switch = int(puertos_switch) + 1
+
+contadorH = 0
+contadorSw = 0
+
+#puertos_switch = math.sqrt(hTotales/2)
+swTotales = (hTotales/(2*puertos_switch))
+
+for h in range(0, hTotales): 
+    print('        H_' + str(h) + ': RdmaHost{};')
+
+    if h < hTotales/2:
+        contador = 1
+    else:
+        contador = 2
+
+    contadorH += 1
+
+    if contadorH == puertos_switch:
+        contadorH = 0
+        contadorSw += 1
+        if contadorSw == puertos_switch + 1:
+            contadorSw = 1
+        print('        SW_' + str(contador) + '_' + str(contadorSw) + ': EthernetSwitch {gates: ethg[' + str(int(puertos_switch*2)) +'];};')
+
+#####################
+#Declaración switches capa intermedia
+for s in range(1,puertos_switch+1):
+
+    print('        SW_3' + '_' + str(s) + ': EthernetSwitch {gates: ethg[' + str(int(puertos_switch*2)) +'];}')
+
+
+#######################################
+##Conexiones
+contadorSw = 1 
+p_sw = 0
+print('connections:')
+
+for h in range(0, hTotales): 
+    if h < hTotales/2:
+        contador = 1
+    else:
+        contador = 2
+
+    contadorH += 1
+    print('        H_' + str(h) + '.ethg++ <--> cable <--> SW_' + str(contador) + '_' + str(contadorSw) + '.ethg[' + str(p_sw)+ '];')
+    p_sw += 1
+    if contadorH == puertos_switch:
+        contadorH = 0
+        contadorSw += 1
+        p_sw =0
+        if contadorSw == puertos_switch + 1:
+            contadorSw = 1
+##############################################
+##Conexiones con los switches de la capa intermedia
+#for num_switch_interno in range(0, puertos_switch):
+ #   for nivel in range(1, 3):
+  #      for num_switch_externo in range(0, puertos_switch):
+   #         print('        SW_3_' +  str(num_switch_interno + 1) + '.ethg[' + str(num_switch_interno + puertos_switch) + '] <--> cable <-->' + 
+#		' SW_' + str(nivel) + '_' + str(num_switch_externo + 1) + ".ethg[" + str(num_switch_interno + puertos_switch) + '];')   
+for internos in range(0, puertos_switch):
+	for nivel in range(1, 3):
+            for externos in range(0, puertos_switch):
+               print('        SW_3_' +  str(internos + 1) + '.ethg[' + str(externos+(nivel-1)*puertos_switch) + '] <--> cable <-->' + 
+		' SW_' + str(nivel) + '_' + str(externos + 1) + ".ethg[" + str(internos + puertos_switch) + '];') 
---
+++ generarTopoManual.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+#
+#
+
+import math
+import sys
+
+if len(sys.argv) != 4:
+        print('El formato de ejecución debe ser ./generarTopo.py host_por_switch num_switches_externos num_switches_internos')
+
+host_switch = int(sys.argv[1]) #Numero de host por switch de la primera etapa
+num_switches_externos = int(sys.argv[2]) #Numero de switches en la primera etapa
+num_switches_internos = int(sys.argv[3]) #Numero de switches en la segunda etapa
+
+if num_switches_internos != 0:
+    puertos_switches_externos = host_switch + num_switches_internos
+else:
+    puertos_switches_externos = host_switch + num_switches_externos
+    
+puertos_switches_internos = num_switches_externos * 2
+host_nivel = host_switch*num_switches_externos*2
+
+print('types:')
+print('        channel cable extends DatarateChannel')
+print('        {')
+print('            datarate = 40Gbps;')
+print('            delay = 1us;')
+print('        }')
+
+print('submodules:')
+
+sw = 0
+for h in range(0, host_nivel): 
+    print('        H_' + str(h) + ': RdmaHost{};')
+    if h < host_nivel/2:
+        grupo = 1
+    else:
+        grupo = 2
+    
+    if ((h + 1) % host_switch) == 0:
+        sw += 1
+        if sw == num_switches_externos + 1:
+            sw = 1
+        print('        SW_' + str(grupo) + '_' + str(sw) + ': EthernetSwitch {gates: ethg[' + str(int(puertos_switches_externos)) +'];};')
+
+for sw3 in range(1, num_switches_internos + 1):
+     print('        SW_3_' + str(sw3) + ': EthernetSwitch {gates: ethg[' + str(int(puertos_switches_internos)) +'];};')
+     
+#########################################################################
+#Conexiones
+p_sw = 0
+sw = 1
+print('connections:')
+for h in range(0,host_nivel):
+    if h < host_nivel/2:
+        grupo = 1
+    else:
+        grupo = 2
+    print('        H_' + str(h) + '.ethg++ <--> cable <--> SW_' + str(grupo) + '_' + str(sw) + '.ethg[' + str(p_sw)+ '];')
+    p_sw += 1
+    if ((h + 1) % host_switch) == 0:
+        sw += 1
+        p_sw = 0
+        if sw == num_switches_externos+ 1:
+            sw = 1
+
+if num_switches_internos != 0:            
+#Conexiones con nivel intermedio
+    for internos in range(0, num_switches_internos):
+        for nivel in range(1, 3):
+            for externos in range(0, num_switches_externos):
+               print('        SW_3_' +  str(internos + 1) + '.ethg[' + str(externos+(nivel-1)*num_switches_externos) + '] <--> cable <-->' + 
+' SW_' + str(nivel) + '_' + str(externos + 1) + ".ethg[" + str(internos + num_switches_externos) + '];') 
+
+else:
+    for externos in range(0, num_switches_externos):
+        for externos2 in range(0, num_switches_externos):
+            print('        SW_1_' +  str(externos2 + 1) + '.ethg[' + str((externos) + host_switch)+ '] <--> cable <--> ' + 'SW_2_' + str(externos + 1) + '.ethg[' + str((externos2) + host_switch) + '];') 
